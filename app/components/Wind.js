568747798b32dad589e87fad8925281fcb08c307
Saiket Das <saiketdas02@gmail.com>
2022-11-16 22:38:31 +0600

    Wind component's AppText implemented

--- app/components/Wind.js
+++ app/components/Wind.js
@@ -1,109 +1,103 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { StyleSheet } from 'react-native'
-import { Image } from 'react-native'
-import Sun from '../assets/icons/wind-power.png'
-
+import { View } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Image } from "react-native";
+import Sun from "../assets/icons/wind-power.png";
+import Text from "./common/AppText";
 
 export default function Wind({ darkMode, weatheInfo }) {
+  let windDirection;
+  if (weatheInfo?.wind?.deg > 270 && weatheInfo?.wind?.deg <= 360) {
+    windDirection = "North";
+  } else if (weatheInfo?.wind?.deg > 180 && weatheInfo?.wind?.deg <= 270) {
+    windDirection = "West";
+  } else if (weatheInfo?.wind?.deg > 90 && weatheInfo?.wind?.deg <= 180) {
+    windDirection = "South";
+  } else if (weatheInfo?.wind?.deg > 0 && weatheInfo?.wind?.deg <= 90) {
+    windDirection = "East";
+  }
 
-    let windDirection;
-    if (weatheInfo?.wind?.deg > 270 && weatheInfo?.wind?.deg <= 360) {
-        windDirection = 'North'
-    }
-    else if (weatheInfo?.wind?.deg > 180 && weatheInfo?.wind?.deg <= 270) {
-        windDirection = 'West'
-    }
+  return (
+    <View
+      style={{
+        ...styles.windCon,
+        backgroundColor: darkMode ? "#fafafa" : "#3e3e42",
+      }}
+    >
+      <Text
+        style={{
+          padding: 15,
+          fontSize: 15,
+          fontWeight: "500",
+          color: darkMode ? "#2f2f2f" : "#FFFFFF",
+        }}
+      >
+        Wind
+      </Text>
 
-    else if (weatheInfo?.wind?.deg > 90 && weatheInfo?.wind?.deg <= 180) {
-        windDirection = 'South'
-    }
+      <View style={{ alignItems: "center" }}>
+        {/* ---------- Linear Circle ---------- */}
 
-    else if (weatheInfo?.wind?.deg > 0 && weatheInfo?.wind?.deg <= 90) {
-        windDirection = 'East'
-    }
+        <View
+          style={{
+            ...styles.windImgCon,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image style={{ width: 90, height: 90 }} source={Sun} />
+        </View>
 
-    return (
-        <View style={{
-            ...styles.windCon,
-            backgroundColor: darkMode ? '#fafafa' : '#3e3e42',
-        }}>
-            <Text style={{
-                ...styles.windText, color: darkMode ? '#2f2f2f' : '#FFFFFF',
-            }}>
-                Wind
+        <View
+          style={{
+            ...styles.windDirectionAndSpeedCon,
+          }}
+        >
+          <View>
+            <Text
+              style={{
+                color: darkMode ? "#2f2f2f" : "#FFFFFF",
+              }}
+            >
+              Direction: {windDirection}
             </Text>
+          </View>
 
-            <View style={{ alignItems: 'center' }}>
-
-                {/* ---------- Linear Circle ---------- */}
-
-                <View style={{
-                    ...styles.windImgCon,
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}>
-                    <Image style={{ width: 90, height: 90, }} source={Sun} />
-
-                </View>
-
-                <View style={{
-                    ...styles.windDirectionAndSpeedCon,
-                }}>
-                    <View>
-                        <Text style={{
-                            color: darkMode ? '#2f2f2f' : '#FFFFFF',
-                        }}>
-                            Direction: {windDirection}
-                        </Text>
-                    </View>
-
-                    <View>
-                        <Text style={{
-                            color: darkMode ? '#2f2f2f' : '#FFFFFF',
-                        }}>
-                            Speed: {Math.round(weatheInfo.wind?.speed * 1.60934) + 'KM/hr'}
-                        </Text>
-                    </View>
-                </View>
-
-            </View >
-
-
-        </View >
-    )
+          <View>
+            <Text
+              style={{
+                color: darkMode ? "#2f2f2f" : "#FFFFFF",
+              }}
+            >
+              Speed: {Math.round(weatheInfo.wind?.speed * 1.60934) + "KM/hr"}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  windCon: {
+    height: 200,
+    borderColor: "#eee",
+    borderRadius: 10,
+    borderTopRightRadius: 45,
+    borderBottomLeftRadius: 40,
+    marginTop: 20,
+  },
 
-    windCon: {
-        height: 200,
-        borderColor: '#eee',
-        borderRadius: 10,
-        borderTopRightRadius: 45,
-        borderBottomLeftRadius: 40,
-        marginTop: 20
-    },
-    windText: {
-        padding: 15,
-        fontSize: 15,
-        fontWeight: '500'
-    },
+  windImgCon: {
+    height: 120,
+    width: 120,
+    // borderWidth: 10,
+  },
 
-    windImgCon: {
-        height: 120,
-        width: 120,
-        // borderWidth: 10,
-    },
-
-
-
-    windDirectionAndSpeedCon: {
-        flexDirection: 'row',
-        marginTop: 2,
-        width: '100%',
-        justifyContent: 'space-around'
-    },
-
-
-})
+  windDirectionAndSpeedCon: {
+    flexDirection: "row",
+    marginTop: 2,
+    width: "100%",
+    justifyContent: "space-around",
+  },
+});
